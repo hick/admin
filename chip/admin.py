136be6c268cb842261da75ba04689c9d9a6aed62
hick <hickwu@qq.com>
2013-08-30 03:28:45 +0800

    + 自动删除等功能

--- chip/admin.py
+++ chip/admin.py
@@ -7,5 +7,5 @@ from chip.models import Chip
 
 # 定制显示列表
 class ChipAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'pub_time', 'created_at', 'source')
+    list_display = ('title', 'author', 'pub_time', 'created_at', 'url')
 admin.site.register(Chip, ChipAdmin)
--- chip/models.py
+++ chip/models.py
@@ -10,18 +10,18 @@ class Chip(models.Model):
 	title = models.CharField(max_length=255)
 	summary = models.CharField(max_length=255, verbose_name='摘要')
 	author = models.CharField(max_length=255, verbose_name='作者')
-	pub_time = models.DateTimeField('发布时间')
+	pub_time = models.DateTimeField('原文发布时间')
 	url  = models.CharField(max_length=255)
 	tags = models.CharField(max_length=255)
 	source = models.IntegerField(default=0)
 	detail = models.TextField(default='')
-	created_at = models.DateTimeField('datetime created')
+	created_at = models.DateTimeField('抓取时间')
 	updated_at = models.DateTimeField('datetime updated')
 	author_url = models.CharField(max_length=255)
 
 	class Meta: 
 		db_table = u'chips'
 
-	## hick 定义显示的列表
-	def __str__(self):
-		return "%s  - %s " % (self.title, self.created_at)
+	## hick 定义显示的列表: admin.py 中有类似 admin.site.register(Chip, ChipAdmin) 的定义则不走这里
+	# def __str__(self):
+	# 	return "%s  - %s " % (self.title, self.created_at)
