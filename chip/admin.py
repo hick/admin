065b92f1646a2a2862324d33b99b0b8bbf63cda1
hick <hickwu@qq.com>
2013-08-30 02:42:50 +0800

    + 定制 django 后台

--- chip/admin.py
+++ chip/admin.py
@@ -1,3 +1,11 @@
+# -*- coding: UTF-8 -*-
 from django.contrib import admin
 from chip.models import Chip
-admin.site.register(Chip)
+
+
+# admin.site.register(Chip)
+
+# 定制显示列表
+class ChipAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_time', 'created_at', 'source')
+admin.site.register(Chip, ChipAdmin)
--- chip/models.py
+++ chip/models.py
@@ -8,9 +8,9 @@ class Chip(models.Model):
 
 	"""创建表结构"""
 	title = models.CharField(max_length=255)
-	summary = models.CharField(max_length=255)
-	author = models.CharField(max_length=255)
-	pub_time = models.DateTimeField('datetime published')
+	summary = models.CharField(max_length=255, verbose_name='摘要')
+	author = models.CharField(max_length=255, verbose_name='作者')
+	pub_time = models.DateTimeField('发布时间')
 	url  = models.CharField(max_length=255)
 	tags = models.CharField(max_length=255)
 	source = models.IntegerField(default=0)
@@ -22,5 +22,6 @@ class Chip(models.Model):
 	class Meta: 
 		db_table = u'chips'
 
+	## hick 定义显示的列表
 	def __str__(self):
-		return self.title
+		return "%s  - %s " % (self.title, self.created_at)
