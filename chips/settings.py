1c6d643dcdc824b2024c5b52f2c72ad97372ebcc
hick <hickwu@qq.com>
2013-09-18 15:39:54 +0800

    + 直连本地

--- chips/settings.py
+++ chips/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'hick',
-        'HOST': '192.168.0.4',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
