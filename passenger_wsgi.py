4bd2c2c4dbeef68799007698c28999f90fc9d4a4
hick <hickwu@qq.com>
2013-10-27 16:31:23 +0800

    + django + python 的初始化

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,6 @@
-import os
-
-def application(environ, start_response):
-  start_response('200 OK', [('Content-Type', 'text/plain')])
-  hickvar = "I am Hick"
-  hickvar = os.sys.version
-  return ["basic passenger for django!!! %s\n" % hickvar]
-
-
+import sys, os
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), '/var/www/django_admin'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chips.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
